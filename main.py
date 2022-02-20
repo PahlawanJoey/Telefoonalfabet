c513daf9c7d50b96f4e165ede20b0c8d69000c67
PahlawanJoey <98671646+PahlawanJoey@users.noreply.github.com>
2022-02-20 20:38:53 +0100

    Update main.py
    update NL met inputerrorfix bij cijfers

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import pandas
 nl_phonetic_dict = pandas.read_csv("nl_phonetic_alphabet.csv")
 nl_phonetic_dict = {row.letter: row.code for (index, row) in nl_phonetic_dict.iterrows()}
-user_input = input("Which word do you need to spell phonetic?: ").upper()
-word = [char for char in user_input]
-spell_word = [nl_phonetic_dict[letter] for letter in word]
-print(spell_word)
+def alfabet():
+    try:
+        user_input = input("Welk woord wil je spellen met het telefoonalfabet?: ").upper()
+        word = [char for char in user_input]
+        spell_word = [nl_phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Graag alleen letters uit het alfabet invoeren")
+        alfabet()
+    else:
+        print(spell_word)
+alfabet()
